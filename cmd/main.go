525e94285a6abc9cad05e6416197caf4b12f085b
arifucoder <arifucoder@gmail.com>
2026-05-23 19:19:11 +0600

    feat: connect with database and inserted data into postgresql db

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v5"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -22,15 +25,25 @@ func (cv *CustomValidator) Validate(i any) error {
 }
 
 type User struct {
-	Name     string `json:"name" validate:"required"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=6"`
+	gorm.Model
+	Name     string `json:"name"     validate:"required"       gorm:"type:varchar(100);not null"`
+	Email    string `json:"email"    validate:"required,email"  gorm:"type:varchar(100);uniqueIndex;not null"`
+	Password string `json:"password"  validate:"required,min=6"  gorm:"type:varchar(255);not null"`
 }
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	dsn := ""
+	dsn := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&User{})
+
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
 
 	e := echo.New()
 	// e.Use(middleware.RequestLogger())
@@ -48,6 +61,12 @@ func main() {
 		}
 		if err = c.Validate(u); err != nil {
 			return err
+		}
+		result := db.Create(u)
+		if result.Error != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]any{
+				"error": result.Error.Error(),
+			})
 		}
 		return c.JSON(http.StatusOK, u)
 	})
